f1562099b44ab2fc2b4f7b66d31eb7e296db5751
Eduardoss45 <temporariac432@gmail.com>
2026-06-30 20:18:34 -0300

    feat(ui): add statistics and service health checks.

--- app/web/src/app/features/orders/services/order.service.ts
+++ app/web/src/app/features/orders/services/order.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, concat, delay, of } from 'rxjs';
 import { Order } from '../models/order.model';
+import { OrdersStatus } from '../../../types';
 
 @Injectable({ providedIn: 'root' })
 export class OrderService {
   private ordersSubject = new BehaviorSubject<Order[]>([]);
   orders$ = this.ordersSubject.asObservable();
 
-  create(order: Order) {
+  private processOrder(id: string) {
+    concat(
+      of<OrdersStatus>('PROCESSING').pipe(delay(2000)),
+      of<OrdersStatus>('PAID').pipe(delay(2000)),
+      of<OrdersStatus>('COMPLETED').pipe(delay(2000)),
+    ).subscribe((status) => {
+      this.updateStatus(id, status);
+    });
+  }
+
+  create(order: Omit<Order, 'id' | 'status' | 'createdAt'>) {
     const newOrder: Order = {
       ...order,
       id: crypto.randomUUID(),
@@ -16,13 +27,21 @@ export class OrderService {
     };
 
     this.ordersSubject.next([...this.ordersSubject.value, newOrder]);
+
+    this.processOrder(newOrder.id);
   }
 
-  updateStatus(id: string, status: Order['status']) {
-    const updated = this.ordersSubject.value.map((order) =>
-      order.id === id ? { ...order, status } : order,
-    );
+  updateStatus(id: string, status: OrdersStatus) {
+    const orders = this.ordersSubject.value.map((order) => {
+      if (order.id === id) {
+        return {
+          ...order,
+          status,
+        };
+      }
+      return order;
+    });
 
-    this.ordersSubject.next(updated);
+    this.ordersSubject.next(orders);
   }
 }
--- app/web/src/app/types/index.ts
+++ app/web/src/app/types/index.ts
@@ -1,1 +1,9 @@
 export type OrdersStatus = 'CREATED' | 'PROCESSING' | 'PAID' | 'FAILED' | 'COMPLETED';
+
+export interface Order {
+    id: string;
+    product: string;
+    quantity: number;
+    status: OrdersStatus;
+    createdAt: number;
+}
